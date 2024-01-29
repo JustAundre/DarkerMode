5863ae6ad22a6728af1c7f0f2f39fa5a67d5e2be
ItsAundre! <126304209+ItsAundre@users.noreply.github.com>
2024-01-29 01:31:08 -0500

    Starting some Javascript.

---
+++ script.js
@@ -0,0 +1,15 @@
+/* What the site will do if the corresponding buttons are clicked. */
+function detailsClick() {
+
+    console.log("The 'Details' button was pressed!")
+}
+
+function permissionsClick() {
+
+    console.log("The 'Permissions' button was pressed!")
+}
+
+function changelogClick() {
+
+    console.log("The 'Changelog' button was pressed!")
+}
