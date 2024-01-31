622c30a2bb891344c6d418f41bce666426e1ff55
ItsAundre! <126304209+ItsAundre@users.noreply.github.com>
2024-01-31 00:44:14 -0500

    Update

--- index.js
+++ index.js
@@ -2,20 +2,45 @@ let packDetails = document.getElementById("packDetailsId");
 let permissions = document.getElementById("termsOfUseId");
 let changelog = document.getElementById("changelogId");
 
+
+
+
 function detailsClick() {
+  // Changes the text to show an indicator of being selected.
   packDetails.textContent = "-Details-";
   permissions.textContent = "Permissions";
   changelog.textContent = "Changelog";
+
+  // Changes the default button css to indicate it is selected.
+  packDetails.setAttribute("active", true)
+  permissions.setAttribute("active", false)
+  changelog.setAttribute("active", false)
 }
 
+
+
 function permissionsClick() {
+    // Changes the text to show an indicator of being selected.
   packDetails.textContent = "Details";
   permissions.textContent = "-Permissions-";
   changelog.textContent = "Changelog";
+
+  // Changes the default button css to indicate it is selected.
+  packDetails.setAttribute("active", false)
+  permissions.setAttribute("active", true)
+  changelog.setAttribute("active", false)
 }
 
+
+
 function changelogClick() {
+    // Changes the text to show an indicator of being selected.
   packDetails.textContent = "Details";
   permissions.textContent = "Permissions";
   changelog.textContent = "-Changelog-";
+
+  // Changes the default button css to indicate it is selected.
+  packDetails.setAttribute("active", false)
+  permissions.setAttribute("active", false)
+  changelog.setAttribute("active", true)
 }
