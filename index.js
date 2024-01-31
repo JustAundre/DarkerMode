6b032b29f48f9594addaeba63318bcf3b6ce02d1
ItsAundre! <126304209+ItsAundre@users.noreply.github.com>
2024-01-31 13:58:45 -0500

    Update

--- index.js
+++ index.js
@@ -5,41 +5,59 @@ let changelog = document.getElementById("changelogId");
 
 
 function detailsClick() {
-  // Changes the text to show an indicator of selection.
+  if (packDetails.hasAttribute("active")) {
+    // Sets the button to inactive if it was already active and was clicked.
+    packDetails.removeAttribute("active")
+    packDetails.textContent = "Details";
+  }
+  else {
+  // Changes the default button CSS to indicate it is selected only if it isn't already selected.
+  packDetails.setAttribute("active", "")
+  permissions.removeAttribute("active")
+  changelog.removeAttribute("active")
+  // Changes the text to show an indicator of selection only if it isn't already selected.
   packDetails.textContent = "-Details-";
   permissions.textContent = "Permissions";
   changelog.textContent = "Changelog";
-
-  // Changes the default button CSS to indicate it is selected.
-  packDetails.setAttribute("active", true)
-  permissions.setAttribute("active", false)
-  changelog.setAttribute("active", false)
+  }
 }
 
 
 
 function permissionsClick() {
-    // Changes the text to show an indicator of selection.
+  if (permissions.hasAttribute("active")) {
+    // Sets the button to inactive if it was already active and was clicked.
+    permissions.removeAttribute("active")
+    permissions.textContent = "Permissions";
+  }
+  else {
+  // Changes the default button CSS to indicate it is selected only if it isn't already selected.
+  packDetails.removeAttribute("active")
+  permissions.setAttribute("active", "")
+  changelog.removeAttribute("active")
+  // Changes the text to show an indicator of selection only if it isn't already selected.
   packDetails.textContent = "Details";
   permissions.textContent = "-Permissions-";
   changelog.textContent = "Changelog";
-
-  // Changes the default button CSS to indicate it is selected.
-  packDetails.setAttribute("active", false)
-  permissions.setAttribute("active", true)
-  changelog.setAttribute("active", false)
+  }
 }
 
 
 
 function changelogClick() {
-    // Changes the text to show an indicator of selection.
+  if (changelog.hasAttribute("active")) {
+    // Sets the button to inactive if it was already active and was clicked.
+    changelog.removeAttribute("active")
+    changelog.textContent = "Changelog";
+  }
+  else {
+  // Changes the default button CSS to indicate it is selected only if it isn't already selected.
+  packDetails.removeAttribute("active")
+  permissions.removeAttribute("active")
+  changelog.setAttribute("active", "")
+  // Changes the text to show an indicator of selection only if it isn't already selected.
   packDetails.textContent = "Details";
   permissions.textContent = "Permissions";
   changelog.textContent = "-Changelog-";
-
-  // Changes the default button CSS to indicate it is selected.
-  packDetails.setAttribute("active", false)
-  permissions.setAttribute("active", false)
-  changelog.setAttribute("active", true)
+  }
 }
