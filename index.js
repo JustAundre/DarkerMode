02185a76996e83b89448a54d26c19ba61c09b612
ItsAundre! <126304209+ItsAundre@users.noreply.github.com>
2024-03-29 11:06:11 -0500

    CSS Update

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
+// Defines elements
 let packDetails = document.getElementById("packDetailsId");
 let permissions = document.getElementById("termsOfUseId");
 let changelog = document.getElementById("changelogId");
 function detailsClick() {
+  // Sets the button to inactive if it was already active and was clicked.
   if (packDetails.hasAttribute("active")) {
-    // Sets the button to inactive if it was already active and was clicked.
     packDetails.removeAttribute("active")
   }
-  else {
   // Changes the default button CSS to indicate it is selected only if it isn't already selected.
+  else {
   packDetails.setAttribute("active", "")
   permissions.removeAttribute("active")
   changelog.removeAttribute("active")
@@ -15,11 +16,9 @@ function detailsClick() {
 }
 function permissionsClick() {
   if (permissions.hasAttribute("active")) {
-    // Sets the button to inactive if it was already active and was clicked.
     permissions.removeAttribute("active")
   }
   else {
-  // Changes the default button CSS to indicate it is selected only if it isn't already selected.
   packDetails.removeAttribute("active")
   permissions.setAttribute("active", "")
   changelog.removeAttribute("active")
@@ -27,11 +26,9 @@ function permissionsClick() {
 }
 function changelogClick() {
   if (changelog.hasAttribute("active")) {
-    // Sets the button to inactive if it was already active and was clicked.
     changelog.removeAttribute("active")
   }
   else {
-  // Changes the default button CSS to indicate it is selected only if it isn't already selected.
   packDetails.removeAttribute("active")
   permissions.removeAttribute("active")
   changelog.setAttribute("active", "")
